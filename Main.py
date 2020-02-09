8652838b7830683cf27b13912d77a4f7d4dc39e4
Alexa <alexa_becker@loomis.org>
2020-02-09 12:00:27 -0500

    fixed buttons part 789

--- Main.py
+++ Main.py
@@ -115,6 +115,246 @@ class StartTextButton(TextButton):
         super().on_release()
         self.action_function()
 
+
+class Halt(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "halt", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Read(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "read", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Write(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "write", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Nop(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "nop", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Setn(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "Setn", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Addn(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "addn", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Copy(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "copy", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Add(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "add", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Sub(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "sub", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Neg(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "neg", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Mul(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "mul", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Div(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "div", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Mod(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "mod", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Jumpn(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "jumpn", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Jumpr(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "jumpr", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Jeqzn(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "jeqzn", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Jnezn(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "jnezn", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Jgtzn(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "jgtzn", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Jltzn(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "jltzn", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Calln(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "calln", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Loadn(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "loadn", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Storen(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "storen", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Loadr(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "loadr", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
+
+class Storer(TextButton):
+    def __init__(self, center_x, center_y, action_function):
+        super().__init__(center_x, center_y, 100, 40, "storer", 18, "Arial")
+        self.action_function = action_function
+
+    def on_release(self):
+        super().on_release()
+        self.action_function()
+
 class MyGame(arcade.Window):
     """
     Main application class.
@@ -139,12 +379,58 @@ class MyGame(arcade.Window):
 
 
     def setup(self):
-
-
         # Create our on-screen GUI buttons
         self.button_list = []
 
         play_button = StartTextButton(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Halt(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Read(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Write(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Nop(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Setn(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Addn(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Copy(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Add(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Sub(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Neg(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Mul(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = div(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Mod(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Jumpn(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Jumpr(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Jeqzn(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Jnezn(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Jgtzn(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Jltzn(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Calln(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Loadn(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Storen(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Loadr(60, 570, self.resume_program)
+        self.button_list.append(play_button)
+        play_button = Storer(60, 570, self.resume_program)
         self.button_list.append(play_button)
 
 
@@ -152,8 +438,6 @@ class MyGame(arcade.Window):
         arcade.start_render()
         for button in self.button_list:
             button.draw()
-
-
 
 
     def on_mouse_press(self, x, y, button, key_modifiers):

fcce70e797b8c7dae696e01c1f08546091aecfd6
PeytonSandridge <peyton.sandridge@gmail.com>
2021-09-04 10:55:56 -0400

    added files

---
+++ AsmBlocks/cmake-build-debug/CMakeFiles/AsmBlocks.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "AsmBlocks.exe"
+  "AsmBlocks.exe.manifest"
+  "AsmBlocks.pdb"
+  "CMakeFiles/AsmBlocks.dir/main.cpp.obj"
+  "libAsmBlocks.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AsmBlocks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
